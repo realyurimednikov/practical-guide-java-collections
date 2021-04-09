4c40bfc90bb922832da88d1e42b997340839a315
realyurimednikov <mednikov.iurii@gmail.com>
2021-04-09 12:34:24 +0200

    Added code examples for streams chapter

---
+++ pgjc/src/test/java/me/mednikov/pgjc/streams/IntermediateOperationsTest.java
@@ -0,0 +1,128 @@
+package me.mednikov.pgjc.streams;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+import me.mednikov.pgjc.common.Department;
+import me.mednikov.pgjc.common.Employee;
+import org.assertj.core.api.Assertions;
+import org.testng.annotations.Test;
+
+class IntermediateOperationsTest {
+    
+    @Test
+    void filterTest(){
+        List<Employee> employees = List.of(
+                new Employee(1, "Mia Smith", Department.MANAGEMENT),
+                new Employee(2, "Ava Williams", Department.HR),
+                new Employee(3, "Isla Johnson", Department.IT),
+                new Employee(4, "Isabella Brown", Department.IT),
+                new Employee(5, "Jessica Williams", Department.MANAGEMENT)
+        );
+        
+        /*
+        Filter number of employees in the IT department
+        */
+        
+        Stream<Employee> stream = employees.stream();
+        
+        long result = stream.filter(emp -> emp.getDepartment() == Department.IT)
+                .count();
+        
+        Assertions.assertThat(result).isEqualTo(2);
+    }
+    
+    @Test
+    void mapTest(){
+        List<Employee> employees = List.of(
+                new Employee(1, "Mia Smith", Department.MANAGEMENT),
+                new Employee(2, "Ava Williams", Department.HR),
+                new Employee(3, "Isla Johnson", Department.IT),
+                new Employee(4, "Isabella Brown", Department.IT),
+                new Employee(5, "Jessica Williams", Department.MANAGEMENT)
+        );
+        
+        /*
+        Map each employee entity as a name string
+        */
+        
+        Stream<Employee> stream = employees.stream();
+        
+        List<String> names = stream.map(emp -> emp.getName())
+                .collect(Collectors.toList());
+        
+        Assertions.assertThat(names).hasSize(5);
+    }
+    
+    @Test
+    void distinctTest(){
+        List<Integer> numbers = List.of(1, 1, 2, 3, 3, 4, 5, 5); 
+        
+        Stream<Integer> stream = numbers.stream();
+        
+        long result = stream.distinct().count();
+        
+        Assertions.assertThat(result).isEqualTo(5);
+    }
+    
+    @Test
+    void sortTest(){
+        List<Integer> numbers = List.of(-9, -18, 0, 25, 4); 
+        
+        Stream<Integer> stream = numbers.stream();
+        
+        List<Integer> result = stream.sorted().collect(Collectors.toList());
+        
+        Assertions.assertThat(result)
+                .containsAll(numbers)
+                .containsExactly(-18, -9, 0, 4, 25);
+    }
+    
+    @Test
+    void whileTest(){
+        Set<Integer> numbers = Set.of(1,2,3,4,5,6,7,8);
+        
+        Stream<Integer> stream = numbers.stream();
+        
+        // don't forget to sort!
+        Set<Integer> result = stream
+                .sorted()
+                .takeWhile(x-> x < 5)
+                .collect(Collectors.toSet());
+        
+        Assertions.assertThat(result).hasSize(4);
+    }
+    
+      
+    @Test
+    void peekTest(){
+        IntStream stream = IntStream.of(2,3,4);
+        
+        int result = stream.peek(n -> {
+            int value = n * 2;
+            System.out.println(value);
+        }).sum();
+        
+        Assertions.assertThat(result).isEqualTo(9);
+        
+    }
+    
+    @Test
+    void peekVsMapTest(){
+        IntStream stream = IntStream.of(2,3,4);
+        
+        int result = stream.peek(n -> {
+            int value = n * 2;
+        }).sum();
+        
+        Assertions.assertThat(result).isEqualTo(9);
+        
+        IntStream stream2 = IntStream.of(2,3,4);
+        
+        int result2 = stream2.map(n -> n * 2).sum();
+        
+        Assertions.assertThat(result2).isEqualTo(18);
+    }
+}
---
+++ pgjc/src/test/java/me/mednikov/pgjc/streams/TerminalOperationsTest.java
@@ -0,0 +1,131 @@
+package me.mednikov.pgjc.streams;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import me.mednikov.pgjc.common.Customer;
+import me.mednikov.pgjc.common.Invoice;
+import org.assertj.core.api.Assertions;
+import org.testng.annotations.Test;
+
+class TerminalOperationsTest {
+    
+    @Test
+    void collectToListTest(){
+        List<Integer> numbers = List.of(2,3,4,5,6,7,8);
+        
+        Stream<Integer> stream = numbers.stream();
+        
+        List<Integer> result = stream
+                .map(n -> n *2)
+                .collect(Collectors.toList());
+        
+        
+        Assertions.assertThat(result).containsExactly(4,6,8,10,12,14,16);
+    }
+    
+    @Test
+    void collectToMapTest(){
+        List<Invoice> invoices = List.of(
+          new Invoice(1, LocalDate.of(2021, Month.MARCH, 10), 
+                  new Customer(1, "Acme Ltd"), BigDecimal.valueOf(1000)),
+          new Invoice(1, LocalDate.of(2021, Month.APRIL, 5), 
+                  new Customer(2, "Rhino Computers Ltd"), BigDecimal.valueOf(350)),
+          new Invoice(1, LocalDate.of(2021, Month.JANUARY, 10), 
+                  new Customer(1, "Acme Ltd"), BigDecimal.valueOf(1000)),
+          new Invoice(1, LocalDate.of(2021, Month.JANUARY, 5), 
+                  new Customer(2, "Rhino Computers Ltd"), BigDecimal.valueOf(400)),
+          new Invoice(1, LocalDate.of(2021, Month.FEBRUARY, 12), 
+                  new Customer(3, "Jack Mozer & Sons"), BigDecimal.valueOf(400)),
+          new Invoice(1, LocalDate.of(2021, Month.FEBRUARY, 10), 
+                  new Customer(1, "Acme Ltd"), BigDecimal.valueOf(900))
+        );
+        
+        Stream<Invoice> stream = invoices.stream();
+        
+        Map<Customer, List<Invoice>> result = stream.collect(
+                Collectors.groupingBy(invoice -> invoice.getCustomer())
+        );
+        
+        Assertions.assertThat(result)
+                .hasSize(3)
+                .containsKey(new Customer(1, "Acme Ltd"))
+                .containsKey(new Customer(2, "Rhino Computers Ltd"))
+                .containsKey(new Customer(3, "Jack Mozer & Sons"));
+    }
+    
+    @Test
+    void findTest(){
+        List<Invoice> invoices = List.of(
+          new Invoice(1, LocalDate.of(2021, Month.MARCH, 10), 
+                  new Customer(1, "Acme Ltd"), BigDecimal.valueOf(1000)),
+          new Invoice(1, LocalDate.of(2021, Month.APRIL, 5), 
+                  new Customer(2, "Rhino Computers Ltd"), BigDecimal.valueOf(350)),
+          new Invoice(1, LocalDate.of(2021, Month.JANUARY, 10), 
+                  new Customer(1, "Acme Ltd"), BigDecimal.valueOf(1000)),
+          new Invoice(1, LocalDate.of(2021, Month.JANUARY, 5), 
+                  new Customer(2, "Rhino Computers Ltd"), BigDecimal.valueOf(400)),
+          new Invoice(1, LocalDate.of(2021, Month.FEBRUARY, 12), 
+                  new Customer(3, "Jack Mozer & Sons"), BigDecimal.valueOf(400)),
+          new Invoice(1, LocalDate.of(2021, Month.FEBRUARY, 10), 
+                  new Customer(1, "Acme Ltd"), BigDecimal.valueOf(900))
+        );
+        
+        Stream<Invoice> stream = invoices.stream();
+        
+        // use filter to find a value
+        Optional<Invoice> result = stream
+                .filter(invoice -> 
+                        invoice.getCustomer()
+                                .equals(new Customer (3, "Jack Mozer & Dons")))
+                .findFirst();
+        
+        Assertions.assertThat(result).isNotEmpty();
+    }
+    
+    @Test
+    void matchTest(){
+        List<Integer> numbers = List.of(10, 53, 23, 95, 30);
+        boolean result = numbers.stream().anyMatch(n -> n%2 == 0);
+        Assertions.assertThat(result).isTrue();
+    }
+    
+    @Test
+    void reduceTest(){
+        List<Invoice> invoices = List.of(
+          new Invoice(1, LocalDate.of(2021, Month.MARCH, 10), 
+                  new Customer(1, "Acme Ltd"), BigDecimal.valueOf(1000)),
+          new Invoice(1, LocalDate.of(2021, Month.APRIL, 5), 
+                  new Customer(2, "Rhino Computers Ltd"), BigDecimal.valueOf(350)),
+          new Invoice(1, LocalDate.of(2021, Month.JANUARY, 10), 
+                  new Customer(1, "Acme Ltd"), BigDecimal.valueOf(1000)),
+          new Invoice(1, LocalDate.of(2021, Month.JANUARY, 5), 
+                  new Customer(2, "Rhino Computers Ltd"), BigDecimal.valueOf(400)),
+          new Invoice(1, LocalDate.of(2021, Month.FEBRUARY, 12), 
+                  new Customer(3, "Jack Mozer & Sons"), BigDecimal.valueOf(400)),
+          new Invoice(1, LocalDate.of(2021, Month.FEBRUARY, 10), 
+                  new Customer(1, "Acme Ltd"), BigDecimal.valueOf(900))
+        );
+        
+        Stream<Invoice> stream = invoices.stream();
+        
+        Map<Month, List<Invoice>> invoicesByMonth = stream.collect(
+                Collectors.groupingBy(invoice -> invoice
+                        .getIssuedDate()
+                        .getMonth()));
+        
+        // use reducation to sum invoices by month
+        invoicesByMonth.forEach((month, invs) -> {
+            BigDecimal total = invs.stream()
+                    .map(i -> i.getTotal())
+                    .reduce(BigDecimal.ZERO, (subtotal, val) -> subtotal.add(val));
+            
+            System.out.println("Total for " + month.name()+ " : " + total.toString());
+        });
+    }
+}
